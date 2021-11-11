11f09397eeac8db9a2a6bce48f3807a685085d75
Nanda Velugoti <nvelugoti@hawk.iit.edu>
2021-11-11 16:52:11 -0600

    Setup proper env variables

---
+++ env_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# export upem-sdk paths
+export PATH=$PATH:$HOME/upmem-sdk/bin
+export CPATH=$CPATH:$HOME/upmem-sdk/include/dpu
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/upmem-sdk/share/pkg-config
+export PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR:$HOME/upmem-sdk/lib64
