afad422671fa611e9f5446a869d1208b4abd0d94
João Dinis Ferreira <hello@joaof.eu>
2021-12-10 06:04:41 +0000

    [VA] Add region markers, fix type warnings, run formatter

--- VA/baselines/cpu/app_baseline.c
+++ VA/baselines/cpu/app_baseline.c
@@ -1,96 +1,106 @@
 /**
-* @file app.c
-* @brief Template for a Host Application Source File.
-*
-*/
+ * @file app.c
+ * @brief Template for a Host Application Source File.
+ *
+ */
+#include <assert.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <assert.h>
-#include <stdint.h>
 
-#include <omp.h>
 #include "../../support/timer.h"
+#include "shared.hpp"
+#include <omp.h>
 
 static int32_t *A;
 static int32_t *B;
 static int32_t *C;
-static int32_t *C2; 
+static int32_t *C2;
 
 /**
-* @brief creates a "test file" by filling a buffer of 64MB with pseudo-random values
-* @param nr_elements how many 32-bit elements we want the file to be
-* @return the buffer address
-*/
-void  *create_test_file(unsigned int nr_elements) {
+ * @brief creates a "test file" by filling a buffer of 64MB with pseudo-random
+ * values
+ * @param nr_elements how many 32-bit elements we want the file to be
+ * @return the buffer address
+ */
+void create_test_file(unsigned int nr_elements) {
     srand(0);
     printf("nr_elements\t%u\t", nr_elements);
-    A = (uint32_t*) malloc(nr_elements * sizeof(uint32_t));
-    B = (uint32_t*) malloc(nr_elements * sizeof(uint32_t));
-    C = (uint32_t*) malloc(nr_elements * sizeof(uint32_t));
-    
-    for (int i = 0; i < nr_elements; i++) {
-        A[i] = (int) (rand());
-        B[i] = (int) (rand());
-    }
+    A = (int32_t *)malloc(nr_elements * sizeof(int32_t));
+    B = (int32_t *)malloc(nr_elements * sizeof(int32_t));
+    C = (int32_t *)malloc(nr_elements * sizeof(int32_t));
 
+    for (int i = 0; i < nr_elements; i++) {
+        A[i] = (int)(rand());
+        B[i] = (int)(rand());
+    }
 }
 
 /**
-* @brief compute output in the host
-*/
+ * @brief compute output in the host
+ */
 static void vector_addition_host(unsigned int nr_elements, int t) {
     omp_set_num_threads(t);
-    #pragma omp parallel for
+#pragma omp parallel for
     for (int i = 0; i < nr_elements; i++) {
+        start_region();
         C[i] = A[i] + B[i];
+        end_region();
     }
 }
 
 // Params ---------------------------------------------------------------------
 typedef struct Params {
-    int   input_size;
-    int   n_warmup;
-    int   n_reps;
-    int   n_threads;
-}Params;
+    int input_size;
+    int n_warmup;
+    int n_reps;
+    int n_threads;
+} Params;
 
 void usage() {
-    fprintf(stderr,
-        "\nUsage:  ./program [options]"
-        "\n"
-        "\nGeneral options:"
-        "\n    -h        help"
-        "\n    -t <T>    # of threads (default=8)"
-        "\n    -w <W>    # of untimed warmup iterations (default=1)"
-        "\n    -e <E>    # of timed repetition iterations (default=3)"
-        "\n"
-        "\nBenchmark-specific options:"
-        "\n    -i <I>    input size (default=8M elements)"
-        "\n");
+    fprintf(stderr, "\nUsage:  ./program [options]"
+                    "\n"
+                    "\nGeneral options:"
+                    "\n    -h        help"
+                    "\n    -t <T>    # of threads (default=8)"
+                    "\n    -w <W>    # of untimed warmup iterations (default=1)"
+                    "\n    -e <E>    # of timed repetition iterations (default=3)"
+                    "\n"
+                    "\nBenchmark-specific options:"
+                    "\n    -i <I>    input size (default=8M elements)"
+                    "\n");
 }
 
 struct Params input_params(int argc, char **argv) {
     struct Params p;
-    p.input_size    = 16777216;
-    p.n_warmup      = 1;
-    p.n_reps        = 3;
-    p.n_threads     = 5;
+    p.input_size = 16777216;
+    p.n_warmup = 1;
+    p.n_reps = 3;
+    p.n_threads = 5;
 
     int opt;
-    while((opt = getopt(argc, argv, "hi:w:e:t:")) >= 0) {
-        switch(opt) {
+    while ((opt = getopt(argc, argv, "hi:w:e:t:")) >= 0) {
+        switch (opt) {
         case 'h':
-        usage();
-        exit(0);
-        break;
-        case 'i': p.input_size    = atoi(optarg); break;
-        case 'w': p.n_warmup      = atoi(optarg); break;
-        case 'e': p.n_reps        = atoi(optarg); break;
-        case 't': p.n_threads        = atoi(optarg); break;
+            usage();
+            exit(0);
+            break;
+        case 'i':
+            p.input_size = atoi(optarg);
+            break;
+        case 'w':
+            p.n_warmup = atoi(optarg);
+            break;
+        case 'e':
+            p.n_reps = atoi(optarg);
+            break;
+        case 't':
+            p.n_threads = atoi(optarg);
+            break;
         default:
             fprintf(stderr, "\nUnrecognized option!\n");
             usage();
@@ -103,8 +113,8 @@ struct Params input_params(int argc, char **argv) {
 }
 
 /**
-* @brief Main of the Host Application.
-*/
+ * @brief Main of the Host Application.
+ */
 int main(int argc, char **argv) {
 
     struct Params p = input_params(argc, argv);
@@ -118,7 +128,7 @@ int main(int argc, char **argv) {
     start(&timer, 0, 0);
 
     vector_addition_host(file_size, p.n_threads);
-	
+
     stop(&timer, 0);
     printf("Kernel ");
     print(&timer, 0, 1);
@@ -128,5 +138,5 @@ int main(int argc, char **argv) {
     free(B);
     free(C);
 
-   return 0;
- }
+    return 0;
+}
